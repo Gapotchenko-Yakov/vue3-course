909e56227e57a84c6b01b30a657c8b0d67cc8e04
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-07-05 11:44:28 +0300

    feat(post): add search input for posts

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
 <div class="app">
     <h1>Страница с постами</h1>
+    <my-input
+        v-model="searchQuery"
+        placeholder="Поиск..."
+    />
     <div class="app_btns">
     <my-button
         @click="showDialog"
@@ -18,7 +22,7 @@
         />
     </my-dialog>
     <post-list
-        :posts="sortedPosts"
+        :posts="sortedAndSearchedPosts"
         @remove="removePost"
         v-if="!isPostsLoading"
     />
@@ -42,6 +46,7 @@ export default {
             dialogVisible: false,
             isPostsLoading: false,
             selectedSort: '',
+            searchQuery: '',
             sortOptions: [
                 {value: 'title', name: 'По названию'},
                 {value: 'body', name: 'По содержанию'},
@@ -85,6 +90,9 @@ export default {
             return [...this.posts].sort((post1, post2) => {
                 return post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]);
             })
+        },
+        sortedAndSearchedPosts(){
+            return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()));
         }
     },
     // watch:{
