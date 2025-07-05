d8596950cd5395da0b893d1e85aff9064065a4bd
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-07-05 13:36:24 +0300

    feat(navbar): add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
+    <navbar />
     <div class="app">
         <router-view></router-view>
     </div>
 </template>
 <script>
+import Navbar from '@/components/Navbar.vue';
+
 export default {
-    
+    components:{
+        Navbar,
+    }
 }
 </script>
 <style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="navbar">
+        <div @click="$router.push('/')">YAGAPP</div>
+        <div class="navbar__btns">
+            <my-button @click="$router.push('/')" class="navbar__btn">Главная</my-button>
+            <my-button @click="$router.push('/about')" class="navbar__btn">О сайте</my-button>
+            <my-button @click="$router.push('/posts')" class="navbar__btn">Посты</my-button>
+        </div>
+    </nav>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    .navbar {
+        height: 50px;
+        background-color: lightgray;
+        box-shadow: 2px 2px 4px gray;
+        display: flex;
+        align-items: center;
+        padding: 0 15px;
+    }
+    .navbar__btns {
+        margin-left: auto;
+    }
+    .navbar__btn {
+        margin-left: 20px;
+    }
+</style>
